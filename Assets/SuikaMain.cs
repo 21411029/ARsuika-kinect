3a5e343cff9b6a63fac2eab89cf5a403d279ce7d
IDEHARA Norimichi <idehara@tama.ac.jp>
2017-03-01 21:49:41 +0900

    Hit Suika, Replace and Fall

---
+++ Assets/SuikaMain.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SuikaMain : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if( col.tag == "Stick")
+            this.transform.position = new Vector3(Random.Range(1.0f, 1.5f), 1.5f, -Random.Range(1.0f, 1.5f));
+    }
+}
